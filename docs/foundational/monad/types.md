528b7128ac035d6e26dd222c456a0589c1cc1fc1
Andric LibreSinn <andric.droid@gmail.com>
2019-11-04 19:25:02 -0500

    adding gen scripts

---
+++ docs/foundational/monad/types.md
@@ -0,0 +1,34 @@
+# Types
+
+* Continuation
+* Either
+* Environment
+* Error
+* Fluture
+* Identity
+* IO
+* List
+* Maybe
+* Reader
+* Result
+* State
+* Writer
+
+<!-- tabs:start -->
+
+#### ** Citations **
+1. Last, First, _[Link Title](Link)_, (YYYY)
+
+#### ** Definition References **
+Full definitions can be found in the [glossary](start-here/glossary)
+
+#### ** References to self **
+* https://rosettacode.org/wiki/Category:Monads
+* https://en.wikipedia.org/wiki/Monad_(functional_programming)#More_examples
+* https://wiki.haskell.org/All_About_Monads
+* https://medium.com/@dtipson/functional-types-in-js-writing-a-writer-3bcd7eee2cb4
+* https://www.humblespark.com/blog/extensible-effects-in-node-part-1
+* https://dev.to/theodesp/explain-monads-like-im-five
+* https://ericlippert.com/category/monads/ (Super long)
+
+<!-- tabs:end -->
--- other/generate-scripts.js
+++ other/generate-scripts.js
@@ -1,11 +1,44 @@
 console.clear()
-const { readdirSync, statSync } = require('fs')
+const { readFileSync, readdirSync, statSync, writeFileSync } = require('fs')
 const { join } = require('path')
 
+// Copied and reformatted from: https://github.com/keithamus/sort-object-keys
+const sortObjectByKeyNameList = (object, sortWith) => {
+  let keys;
+  let sortFn;
+
+  if (typeof sortWith === 'function') {
+    sortFn = sortWith;
+  } else {
+    keys = sortWith;
+  }
+  
+  return (keys || []).concat(Object.keys(object).sort(sortFn)).reduce(function(total, key) {
+    total[key] = object[key];
+    return total;
+  }, Object.create(null));
+}
+
 const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
-const script = (dir) => `"test:${dir}": "mocha --require @babel/register --recursive ./src/${dir} --reporter mocha-clearscreen-reporter --watch"`
-const scripts = dirs('./src').map((dir) => script(dir)).join(', \n')
 
-console.log(scripts)
+const script = (dir) => ({
+	key: `test:${dir}`,
+	value: `mocha --require @babel/register --recursive ./src/${dir} --reporter mocha-clearscreen-reporter --watch`
+})
 
+const cata = (acc, dir) => ({
+	[script(dir).key]: script(dir).value,
+	...acc
+})
 
+const scripts = dirs('./src').reduce(cata, {})
+const packageConfig = readFileSync('./package.json')
+const packageAsJSON = JSON.parse(packageConfig)
+
+const allScripts = {
+	...scripts,
+	...packageAsJSON.scripts,
+}
+
+packageAsJSON.scripts = sortObjectByKeyNameList(allScripts)
+writeFileSync('./package.json', JSON.stringify(packageAsJSON, null, '  '))
